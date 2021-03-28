b462fbc42247cc58cfdfa9c86811fdbac76a461c
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-03-28 08:45:51 +0200

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -63,6 +63,7 @@ echo "Port $sshport
 HostKey /etc/ssh/key1rsa
 HostKey /etc/ssh/key2ecdsa
 HostKey /etc/ssh/key3ed25519
+macs hmac-sha2-256,hmac-sha2-512,umac-128@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
 PermitRootLogin yes
 PasswordAuthentication yes
 ChallengeResponseAuthentication no
