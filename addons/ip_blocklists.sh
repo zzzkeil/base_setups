099aaf970f02e4857dcca3fdfca6a588bc04a304
Klaus <38099918+zzzkeil@users.noreply.github.com>
2022-01-02 21:04:15 +0100

    Update ip_blocklists.sh

--- addons/ip_blocklists.sh
+++ addons/ip_blocklists.sh
@@ -17,8 +17,8 @@ echo -e " ${GRAYB}##############################################################
 echo -e " ${GRAYB}#${ENDCOLOR} ${RED} TEST script -- not finished -- just for testing -- high risk to suck        ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
 echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
 echo -e " ${GRAYB}#${ENDCOLOR} ${YELLOW}This script will block all traffic from ip´s in the following blocklist   ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
-echo -e " ${GRAYB}#${ENDCOLOR} ${YELLOW}Blocklist URL = https://iplists.firehol.org/files/firehol_level1.netset   ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
-echo -e " ${GRAYB}#${ENDCOLOR} ${YELLOW}Autoupdate list and blocks every 6 hours                                  ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${YELLOW}Blocklist URL = ??? https://www.darklist.de/iptables.php                  ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${YELLOW}Autoupdate list and blocks every day                                      ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
 echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
 echo -e " ${GRAYB}#${ENDCOLOR}                 Version 2022.01.02 - changelog on github                   ${GRAYB}#${ENDCOLOR}"
 echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
@@ -45,18 +45,26 @@ fi
 
 
 
-### create crontabs
-
-(crontab -l ; echo "33 0,6,12,18 * * * cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw delete deny from {}") | sort - | uniq - | crontab -
-(crontab -l ; echo "37 0,6,12,18 * * * curl -o /root/firehol_level1.netset https://iplists.firehol.org/files/firehol_level1.netset") | sort - | uniq - | crontab -
-(crontab -l ; echo "40 0,6,12,18 * * * cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw deny from {} to any") | sort - | uniq - | crontab -
 
 
 
-curl -o /root/firehol_level1.netset https://iplists.firehol.org/files/firehol_level1.netset
 
-cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw deny from {} to any
 
-cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw delete deny from {}
+
+#!! not practical
+
+#### create crontabs
+
+#(crontab -l ; echo "33 0,6,12,18 * * * cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw delete deny from {}") | sort - | uniq - | crontab -
+#(crontab -l ; echo "37 0,6,12,18 * * * curl -o /root/firehol_level1.netset https://iplists.firehol.org/files/firehol_level1.netset") | sort - | uniq - | crontab -
+#(crontab -l ; echo "40 0,6,12,18 * * * cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw deny from {} to any") | sort - | uniq - | crontab -
+
+
+
+#curl -o /root/firehol_level1.netset https://iplists.firehol.org/files/firehol_level1.netset
+
+#cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw deny from {} to any
+
+#cat /root/firehol_level1.netset  | awk '/^[^#]/ { print $1 }' | sudo xargs -I {} ufw delete deny from {}
 
 
