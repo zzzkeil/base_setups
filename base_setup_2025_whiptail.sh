4b9be722da38e7bf5adc99cb3405fd03e1ea465c
keil <38099918+zzzkeil@users.noreply.github.com>
2025-08-18 14:07:37 +0200

    Update base_setup_2025_whiptail.sh

--- base_setup_2025_whiptail.sh
+++ base_setup_2025_whiptail.sh
@@ -132,7 +132,7 @@ install_multiple_packages_with_gauge1() {
         done
         echo 100
         echo "All packages installed successfully."
-    } | whiptail --title "Installing needed OS Packages" --gauge "Please wait while installing packages...\n$packages1" 15 90 0
+    } | whiptail --title "Installing needed OS Packages" --gauge "Please wait while installing packages...\nfirewalld, fail2ban, rsyslog, unattended-upgrades, apt-listchanges" 15 90 0
 
     if [ $? -eq 0 ]; then
         echo ""
